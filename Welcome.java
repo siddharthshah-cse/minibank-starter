64374b9c18cf80bc1ca90caf1d4f616b5caac491
siddharthshah-cse <siddharthshah.cse@charusat.ac.in>
2026-06-20 16:45:52 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+// Run this once to confirm your Codespace and Java are working,
+// then delete it and start Lab 1.
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("MiniBank environment is ready.");
+        System.out.println("Java " + System.getProperty("java.version") + " is working.");
+        System.out.println("You can start Lab 1.");
+    }
+}
